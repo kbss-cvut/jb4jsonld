cec20fdcf287c869d8e88980e639146a442f6fc2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-04-16 17:16:07 +0200

    Allow to specify deserialization order of attributes.

--- src/main/java/cz/cvut/kbss/jsonld/deserialization/expanded/ObjectDeserializer.java
+++ src/main/java/cz/cvut/kbss/jsonld/deserialization/expanded/ObjectDeserializer.java
@@ -1,11 +1,13 @@
 package cz.cvut.kbss.jsonld.deserialization.expanded;
 
 import cz.cvut.kbss.jsonld.ConfigParam;
+import cz.cvut.kbss.jsonld.common.BeanAnnotationProcessor;
 import cz.cvut.kbss.jsonld.deserialization.InstanceBuilder;
+import cz.cvut.kbss.jsonld.exception.JsonLdDeserializationException;
 import cz.cvut.kbss.jsonld.exception.UnknownPropertyException;
 
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 class ObjectDeserializer extends Deserializer<Map<?, ?>> {
 
@@ -29,7 +31,7 @@ class ObjectDeserializer extends Deserializer<Map<?, ?>> {
     @Override
     void processValue(Map<?, ?> value) {
         openObject(value);
-        for (Map.Entry<?, ?> e : value.entrySet()) {
+        for (Map.Entry<?, ?> e : orderAttributesForProcessing(value).entrySet()) {
             final String property = e.getKey().toString();
             final boolean shouldSkip = shouldSkipProperty(property);
             if (shouldSkip) {
@@ -54,6 +56,49 @@ class ObjectDeserializer extends Deserializer<Map<?, ?>> {
             assert targetClass.isAssignableFrom(cls);
             instanceBuilder.openObject(cls);
         }
+    }
+
+    private Map<?, ?> orderAttributesForProcessing(Map<?, ?> value) {
+        final List<String> propertyOrder = getPropertyOrder();
+        if (propertyOrder.isEmpty()) {
+            return value;
+        }
+        final Map result = new LinkedHashMap<>(value.size());
+        for (String property : propertyOrder) {
+            final Iterator<? extends Map.Entry<?, ?>> it = value.entrySet().iterator();
+            while (it.hasNext()) {
+                final Map.Entry<?, ?> e = it.next();
+                if (property.equals(e.getKey())) {
+                    result.put(e.getKey(), e.getValue());
+                    it.remove();
+                    break;
+                }
+            }
+        }
+        result.putAll(value);
+        return result;
+    }
+
+    private List<String> getPropertyOrder() {
+        final Class<?> cls = instanceBuilder.getCurrentContextType();
+        if (cls == null) {
+            return Collections.emptyList();
+        }
+        final String[] attributeOrder = BeanAnnotationProcessor.getAttributeOrder(cls);
+        if (attributeOrder.length == 0) {
+            return Collections.emptyList();
+        }
+        final List<Field> fields = BeanAnnotationProcessor.getSerializableFields(instanceBuilder.getCurrentRoot());
+        final List<String> propertyOrder = new ArrayList<>(attributeOrder.length);
+        for (String name : attributeOrder) {
+            final Optional<Field> field = fields.stream().filter(f -> f.getName().equals(name)).findFirst();
+            if (!field.isPresent()) {
+                throw new JsonLdDeserializationException(
+                        "Field called " + name + " declared in JsonLdAttributeOrder annotation not found in class " + cls + " .");
+            }
+            propertyOrder.add(BeanAnnotationProcessor.getAttributeIdentifier(field.get()));
+        }
+        return propertyOrder;
     }
 
     private boolean shouldSkipProperty(String property) {
---
+++ src/test/java/cz/cvut/kbss/jsonld/deserialization/expanded/ObjectDeserializerTest.java
@@ -0,0 +1,101 @@
+package cz.cvut.kbss.jsonld.deserialization.expanded;
+
+import cz.cvut.kbss.jopa.CommonVocabulary;
+import cz.cvut.kbss.jopa.model.annotations.Id;
+import cz.cvut.kbss.jopa.model.annotations.OWLAnnotationProperty;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jsonld.Configuration;
+import cz.cvut.kbss.jsonld.JsonLd;
+import cz.cvut.kbss.jsonld.annotation.JsonLdAttributeOrder;
+import cz.cvut.kbss.jsonld.deserialization.InstanceBuilder;
+import cz.cvut.kbss.jsonld.deserialization.util.TargetClassResolver;
+import cz.cvut.kbss.jsonld.environment.TestUtil;
+import cz.cvut.kbss.jsonld.environment.Vocabulary;
+import cz.cvut.kbss.jsonld.environment.model.Employee;
+import cz.cvut.kbss.jsonld.environment.model.Study;
+import cz.cvut.kbss.jsonld.exception.JsonLdDeserializationException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Matchers.anyCollection;
+import static org.mockito.Mockito.*;
+
+public class ObjectDeserializerTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Mock
+    private InstanceBuilder instanceBuilderMock;
+
+    @Mock
+    private TargetClassResolver tcResolverMock;
+
+    private ObjectDeserializer deserializer;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void processValueProcessesAttributesInOrderSpecifiedByJsonLdAttributeOrder() throws Exception {
+        when(tcResolverMock.getTargetClass(eq(Study.class), anyCollection())).thenReturn(Study.class);
+        when(tcResolverMock.getTargetClass(eq(Employee.class), anyCollection())).thenReturn(Employee.class);
+        when(instanceBuilderMock.getCurrentRoot()).thenReturn(new Study());
+        when(instanceBuilderMock.isPropertyMapped(any())).thenReturn(true);
+        when(instanceBuilderMock.isPlural(Vocabulary.HAS_MEMBER)).thenReturn(true);
+        when(instanceBuilderMock.isPlural(Vocabulary.HAS_PARTICIPANT)).thenReturn(true);
+        doAnswer(inv -> Study.class).when(instanceBuilderMock).getCurrentContextType();
+        doAnswer(inv -> Employee.class).when(instanceBuilderMock).getCurrentCollectionElementType();
+        this.deserializer =
+                new ObjectDeserializer(instanceBuilderMock, new DeserializerConfig(new Configuration(), tcResolverMock),
+                        Study.class);
+        final List<?> input = (List<?>) TestUtil.readAndExpand("objectWithPluralReferenceSharingObject.json");
+        deserializer.processValue((Map<?, ?>) input.get(0));
+
+        final InOrder inOrder = inOrder(instanceBuilderMock);
+        inOrder.verify(instanceBuilderMock).addValue(eq(JsonLd.ID), any());
+        inOrder.verify(instanceBuilderMock).addValue(eq(CommonVocabulary.RDFS_LABEL), any());
+        inOrder.verify(instanceBuilderMock).openCollection(Vocabulary.HAS_PARTICIPANT);
+        inOrder.verify(instanceBuilderMock).openCollection(Vocabulary.HAS_MEMBER);
+    }
+
+    @Test
+    public void processValueThrowsJsonLdDeserializationExceptionWhenUnknownFieldNameIsUsedInAttributeOrderSpecification() throws
+                                                                                                                          Exception {
+        when(tcResolverMock.getTargetClass(eq(InvalidOrder.class), anyCollection())).thenReturn(InvalidOrder.class);
+        when(instanceBuilderMock.getCurrentRoot()).thenReturn(new InvalidOrder());
+        when(instanceBuilderMock.isPropertyMapped(any())).thenReturn(true);
+        doAnswer(inv -> InvalidOrder.class).when(instanceBuilderMock).getCurrentContextType();
+        this.deserializer =
+                new ObjectDeserializer(instanceBuilderMock, new DeserializerConfig(new Configuration(), tcResolverMock),
+                        InvalidOrder.class);
+        final List<?> input = (List<?>) TestUtil.readAndExpand("objectWithPluralReferenceSharingObject.json");
+
+        thrown.expect(JsonLdDeserializationException.class);
+        thrown.expectMessage(
+                "Field called unknown declared in JsonLdAttributeOrder annotation not found in class " + InvalidOrder.class + " .");
+        deserializer.processValue((Map<?, ?>) input.get(0));
+    }
+
+    @OWLClass(iri = Vocabulary.ORGANIZATION)
+    @JsonLdAttributeOrder({"unknown", "name"})
+    public static class InvalidOrder {
+
+        @Id
+        private URI uri;
+
+        @OWLAnnotationProperty(iri = CommonVocabulary.RDFS_LABEL)
+        private String name;
+    }
+}
