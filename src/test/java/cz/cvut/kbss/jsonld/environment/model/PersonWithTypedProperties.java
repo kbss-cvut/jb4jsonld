93984dd9e6308609bdbb221de8e70a304a0a4ff8
Martin Ledvinka <martin.ledvinka@outlook.com>
2020-12-23 10:09:16 +0100

    [Bug #22] Add tests reproducing the issue.

---
+++ src/test/java/cz/cvut/kbss/jsonld/environment/model/PersonWithTypedProperties.java
@@ -0,0 +1,59 @@
+package cz.cvut.kbss.jsonld.environment.model;
+
+import cz.cvut.kbss.jopa.model.annotations.Id;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.Properties;
+import cz.cvut.kbss.jsonld.environment.Vocabulary;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Set;
+
+@OWLClass(iri = Vocabulary.PERSON)
+public class PersonWithTypedProperties {
+
+    @Id
+    public URI uri;
+
+    @OWLDataProperty(iri = Vocabulary.FIRST_NAME)
+    private String firstName;
+
+    @OWLDataProperty(iri = Vocabulary.LAST_NAME)
+    private String lastName;
+
+    @Properties
+    private Map<URI, Set<Object>> properties;
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<URI, Set<Object>> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<URI, Set<Object>> properties) {
+        this.properties = properties;
+    }
+}
