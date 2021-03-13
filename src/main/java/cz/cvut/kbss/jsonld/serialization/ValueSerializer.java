e061f91d9261a5a10bcb3fbdf19ea3d2cea0aa21
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-13 16:51:03 +0100

    [Feature #28] Implement support for custom serializers of data and annotation property values.

--- src/main/java/cz/cvut/kbss/jsonld/serialization/ValueSerializer.java
+++ src/main/java/cz/cvut/kbss/jsonld/serialization/ValueSerializer.java
@@ -1,35 +1,32 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jsonld.serialization;
 
 import cz.cvut.kbss.jsonld.serialization.model.JsonNode;
-
-import java.lang.reflect.Field;
-import java.util.List;
+import cz.cvut.kbss.jsonld.serialization.traversal.SerializationContext;
 
 /**
- * Serializes value.
+ * Serializes values.
  */
-interface ValueSerializer {
+interface ValueSerializer<T> {
 
     /**
      * Serializes the specified value, returning a JSON-LD node representing it.
+     * <p>
+     * Note that if the value is a singular, the returned node should also contain the identifier of the serialized attribute (available
+     * through the provided serialization context).
      *
-     * @param attId Attribute identifier
      * @param value Value to serialize
+     * @param ctx   Serialization context
      * @return Serialization result
      */
-    JsonNode serialize(String attId, Object value);
+    JsonNode serialize(T value, SerializationContext<T> ctx);
 }
---
+++ src/test/java/cz/cvut/kbss/jsonld/serialization/CustomLiteralSerializersTest.java
@@ -0,0 +1,45 @@
+package cz.cvut.kbss.jsonld.serialization;
+
+import cz.cvut.kbss.jsonld.environment.Generator;
+import cz.cvut.kbss.jsonld.environment.Vocabulary;
+import cz.cvut.kbss.jsonld.environment.model.Study;
+import cz.cvut.kbss.jsonld.serialization.model.JsonNode;
+import cz.cvut.kbss.jsonld.serialization.traversal.SerializationContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CustomLiteralSerializersTest {
+
+    private ValueSerializers serializers;
+
+    private JsonLdTreeBuilder treeBuilder;
+
+    @BeforeEach
+    void setUp() {
+        this.serializers = new ValueSerializers();
+        this.treeBuilder = new JsonLdTreeBuilder(serializers);
+    }
+
+    @Test
+    void visitAttributeUsesConfiguredCustomSerializerWhenTypeMatches() throws Exception {
+        final ValueSerializer<Integer> intSerializer = mock(ValueSerializer.class);
+        serializers.registerSerializer(Integer.class, intSerializer);
+        final Study study = new Study();
+        study.setUri(Generator.generateUri());
+        study.setNoOfPeopleInvolved(10);
+        final JsonNode serialized = JsonNodeFactory.createLiteralNode(Vocabulary.NUMBER_OF_PEOPLE_INVOLVED, (long) study.getNoOfPeopleInvolved());
+        when(intSerializer.serialize(any(), any())).thenReturn(serialized);
+        treeBuilder.openObject(new SerializationContext<>(study));
+        final SerializationContext<Integer> ctx = new SerializationContext<Integer>(Vocabulary.NUMBER_OF_PEOPLE_INVOLVED, Study.class.getDeclaredField("noOfPeopleInvolved"), study.getNoOfPeopleInvolved());
+        treeBuilder.visitAttribute(ctx);
+        verify(intSerializer).serialize(study.getNoOfPeopleInvolved(), ctx);
+        final JsonNode node = JsonLdTreeBuilderTest.getNode(treeBuilder.getTreeRoot(), Vocabulary.NUMBER_OF_PEOPLE_INVOLVED);
+        assertNotNull(node);
+        assertSame(serialized, node);
+    }
+}
