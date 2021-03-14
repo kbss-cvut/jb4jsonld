5900c6f5c4987db4ad4f692b2e51e5b22194ecc4
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2021-03-14 10:39:35 +0100

    [Feature #22] Allow registering custom serializers.

--- src/main/java/cz/cvut/kbss/jsonld/serialization/JsonLdSerializer.java
+++ src/main/java/cz/cvut/kbss/jsonld/serialization/JsonLdSerializer.java
@@ -1,16 +1,12 @@
 /**
  * Copyright (C) 2020 Czech Technical University in Prague
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details. You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.jsonld.serialization;
 
@@ -25,8 +21,7 @@ import java.util.Objects;
 /**
  * Base class for all JSON-LD serializers.
  * <p>
- * The serializers will mostly differ in the form of the generated JSON. E.g. the output can be expanded, using contexts
- * etc.
+ * The serializers will mostly differ in the form of the generated JSON. E.g. the output can be expanded, using contexts etc.
  */
 public abstract class JsonLdSerializer implements Configurable {
 
@@ -35,6 +30,8 @@ public abstract class JsonLdSerializer implements Configurable {
     final ObjectGraphTraverser traverser = new ObjectGraphTraverser();
 
     final JsonGenerator jsonGenerator;
+
+    final ValueSerializers serializers = new ValueSerializers();
 
     protected JsonLdSerializer(JsonGenerator jsonGenerator) {
         this.jsonGenerator = Objects.requireNonNull(jsonGenerator);
@@ -52,10 +49,25 @@ public abstract class JsonLdSerializer implements Configurable {
     }
 
     /**
+     * Registers a custom serializer for the specified type.
+     * <p>
+     * If a serializer already existed for the type, it is replaced by the new one.
+     *
+     * @param type       Type to register the serializer for
+     * @param serializer Serializer to register
+     * @param <T>        Serialized type
+     */
+    public <T> void registerSerializer(Class<T> type, ValueSerializer<T> serializer) {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(serializer);
+        serializers.registerSerializer(type, serializer);
+    }
+
+    /**
      * Serializes object graph with the specified root.
      * <p>
-     * The serialization builds a JSON-LD tree model and then writes it using a {@link JsonGenerator}, which was
-     * passed to this instance in constructor.
+     * The serialization builds a JSON-LD tree model and then writes it using a {@link JsonGenerator}, which was passed to this instance in
+     * constructor.
      *
      * @param root Object graph root
      */
