9544e89d9b98c8b21c66e82e5c043f6b39c77909
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2022-03-16 16:45:26 +0100

    [Feature #28] Add interface and management for custom deserializers.

---
+++ src/main/java/cz/cvut/kbss/jsonld/deserialization/CommonValueDeserializers.java
@@ -0,0 +1,39 @@
+package cz.cvut.kbss.jsonld.deserialization;
+
+import cz.cvut.kbss.jsonld.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Manages deserializers for one deserialization process.
+ */
+public class CommonValueDeserializers implements ValueDeserializers {
+
+    private final Map<Class<?>, ValueDeserializer<?>> deserializers = new HashMap<>();
+
+    @Override
+    public <T> boolean hasCustomDeserializer(Class<T> type) {
+        return deserializers.containsKey(type);
+    }
+
+    @Override
+    public <T> Optional<ValueDeserializer<T>> getSerializer(DeserializationContext<T> ctx) {
+        Objects.requireNonNull(ctx);
+        return Optional.ofNullable((ValueDeserializer<T>) deserializers.get(ctx.getTargetType()));
+    }
+
+    @Override
+    public <T> void registerDeserializer(Class<T> forType, ValueDeserializer<T> deserializer) {
+        Objects.requireNonNull(forType);
+        Objects.requireNonNull(deserializer);
+        deserializers.put(forType, deserializer);
+    }
+
+    @Override
+    public void configure(Configuration configuration) {
+        deserializers.values().forEach(dsr -> dsr.configure(configuration));
+    }
+}
---
+++ src/main/java/cz/cvut/kbss/jsonld/deserialization/ValueDeserializers.java
@@ -0,0 +1,38 @@
+package cz.cvut.kbss.jsonld.deserialization;
+
+import cz.cvut.kbss.jsonld.common.Configurable;
+
+import java.util.Optional;
+
+/**
+ * Manages custom deserializers.
+ */
+public interface ValueDeserializers extends Configurable {
+
+    /**
+     * Checks whether a custom deserializer is registered for the specified type.
+     *
+     * @param type Type to check for custom deserializer for
+     * @param <T>  Type of value
+     * @return Whether a custom deserializer exists
+     */
+    <T> boolean hasCustomDeserializer(Class<T> type);
+
+    /**
+     * Gets a custom deserializer for the specified deserialization context.
+     *
+     * @param ctx Context representing the deserialization
+     * @param <T> Type of the value
+     * @return Optional containing the custom deserializer registered for the specified type or an empty optional if there is none
+     */
+    <T> Optional<ValueDeserializer<T>> getSerializer(DeserializationContext<T> ctx);
+
+    /**
+     * Registers the specified deserializer for the specified type.
+     *
+     * @param forType      Type to be deserialized using the specified deserializer
+     * @param deserializer Deserializer to register
+     * @param <T>          Value type
+     */
+    <T> void registerDeserializer(Class<T> forType, ValueDeserializer<T> deserializer);
+}
