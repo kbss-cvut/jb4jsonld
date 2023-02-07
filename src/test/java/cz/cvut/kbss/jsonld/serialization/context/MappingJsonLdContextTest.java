37ad9933550720e9bb6a538e118c25bf7581045e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-02-07 12:30:57 +0100

    [Enhancement #43] Support chaining JSON-LD contexts into hierarchy.

--- src/test/java/cz/cvut/kbss/jsonld/serialization/context/MappingJsonLdContextTest.java
+++ src/test/java/cz/cvut/kbss/jsonld/serialization/context/MappingJsonLdContextTest.java
@@ -1,9 +1,12 @@
 package cz.cvut.kbss.jsonld.serialization.context;
 
 import cz.cvut.kbss.jopa.vocabulary.DC;
+import cz.cvut.kbss.jopa.vocabulary.RDFS;
 import cz.cvut.kbss.jopa.vocabulary.XSD;
 import cz.cvut.kbss.jsonld.JsonLd;
 import cz.cvut.kbss.jsonld.environment.Generator;
+import cz.cvut.kbss.jsonld.serialization.JsonNodeFactory;
+import cz.cvut.kbss.jsonld.serialization.model.JsonNode;
 import cz.cvut.kbss.jsonld.serialization.serializer.SerializerUtils;
 import org.junit.jupiter.api.Test;
 
@@ -42,5 +45,32 @@ class MappingJsonLdContextTest {
         final Optional<String> result = sut.getMappedTerm(Generator.generateUri().toString());
         assertNotNull(result);
         assertFalse(result.isPresent());
+    }
+
+    @Test
+    void getTermMappingRetrievesMappingFromParentContextWhenAvailable() {
+        sut.registerTermMapping("id", JsonLd.ID);
+        final MappingJsonLdContext currentSut = new MappingJsonLdContext(sut);
+        final Optional<JsonNode> result = currentSut.getTermMapping("id");
+        assertTrue(result.isPresent());
+        assertEquals(JsonNodeFactory.createStringLiteralNode("id", JsonLd.ID), result.get());
+    }
+
+    @Test
+    void getTermMappingReturnsMappingFromCurrentContextThatOverridesParentMapping() {
+        final String term = "name";
+        sut.registerTermMapping(term, RDFS.LABEL);
+        final MappingJsonLdContext currentSut = new MappingJsonLdContext(sut);
+        currentSut.registerTermMapping(term, DC.Terms.TITLE);
+        final Optional<JsonNode> result = currentSut.getTermMapping(term);
+        assertTrue(result.isPresent());
+        assertEquals(JsonNodeFactory.createStringLiteralNode(term, DC.Terms.TITLE), result.get());
+    }
+
+    @Test
+    void hasTermMappingReturnsTrueWhenTermIsMappedInParentContext() {
+        sut.registerTermMapping("id", JsonLd.ID);
+        final MappingJsonLdContext currentSut = new MappingJsonLdContext(sut);
+        assertTrue(currentSut.hasTermMapping("id"));
     }
 }
