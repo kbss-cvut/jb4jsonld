a224325eb4a11a88c74cd925c99a4654e9cd9672
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-02-08 13:23:05 +0100

    [Enhancement #43] Do not replicate inherited term mapping when overriding terms in embedded context.

--- src/test/java/cz/cvut/kbss/jsonld/serialization/context/EmbeddedTermMappingHolderTest.java
+++ src/test/java/cz/cvut/kbss/jsonld/serialization/context/EmbeddedTermMappingHolderTest.java
@@ -6,6 +6,9 @@ import cz.cvut.kbss.jsonld.exception.AmbiguousTermMappingException;
 import cz.cvut.kbss.jsonld.serialization.JsonNodeFactory;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmbeddedTermMappingHolderTest {
@@ -21,4 +24,13 @@ class EmbeddedTermMappingHolderTest {
                                                    JsonNodeFactory.createStringLiteralNode(term, DC.Terms.TITLE)));
     }
 
+    @Test
+    void registerTermMappingDoesNothingWhenParentContextAlreadyHasEquivalentMappingForSpecifiedTerm() {
+        final EmbeddedTermMappingHolder child = new EmbeddedTermMappingHolder(sut);
+        final String term = "name";
+        sut.registerTermMapping(term, JsonNodeFactory.createStringLiteralNode(term, RDFS.LABEL));
+        child.registerTermMapping(term, JsonNodeFactory.createStringLiteralNode(term, RDFS.LABEL));
+        assertThat(child.getMapping(), not(hasKey(term)));
+        assertTrue(child.hasTermMapping(term));
+    }
 }
