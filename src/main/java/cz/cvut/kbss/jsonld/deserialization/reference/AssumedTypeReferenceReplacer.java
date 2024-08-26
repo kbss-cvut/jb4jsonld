cfcb2b2999e4a2fab20b8a0be95819801a327c8c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2024-08-26 16:08:38 +0200

    [Enhancement #69] Replace pending references with empty object (with id) when ASSUME_TARGET_TYPE is configured.

---
+++ src/main/java/cz/cvut/kbss/jsonld/deserialization/reference/AssumedTypeReferenceReplacer.java
@@ -0,0 +1,71 @@
+package cz.cvut.kbss.jsonld.deserialization.reference;
+
+import cz.cvut.kbss.jsonld.JsonLd;
+import cz.cvut.kbss.jsonld.common.BeanAnnotationProcessor;
+import cz.cvut.kbss.jsonld.common.BeanClassProcessor;
+import cz.cvut.kbss.jsonld.deserialization.util.DataTypeTransformer;
+import cz.cvut.kbss.jsonld.exception.UnknownPropertyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Replaces pending references with objects of assumed target type.
+ * <p>
+ * These objects have only identifier value set.
+ */
+public class AssumedTypeReferenceReplacer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AssumedTypeReferenceReplacer.class);
+
+    /**
+     * Replaces pending references from the specified {@link PendingReferenceRegistry} with empty objects of the assumed
+     * target type.
+     * <p>
+     * The objects will have only the identifiers set.
+     * <p>
+     * If unable to determine target type (typically for pending collection item references), the pending reference is
+     * skipped.
+     *
+     * @param registry Registry from which the pending references should be replaced
+     */
+    public void replacePendingReferencesWithAssumedTypedObjects(PendingReferenceRegistry registry) {
+        Objects.requireNonNull(registry);
+        final Map<String, Class<?>> idsToTypes = getAssumedTargetTypes(registry);
+        idsToTypes.forEach((id, type) -> {
+            final Object instance = BeanClassProcessor.createInstance(type);
+            final Optional<Field> idField = BeanAnnotationProcessor.getIdentifierField(type);
+            if (idField.isEmpty()) {
+                throw UnknownPropertyException.create(JsonLd.ID, type);
+            }
+            Object identifier = id;
+            if (!idField.get().getType().isAssignableFrom(String.class)) {
+                identifier = DataTypeTransformer.transformValue(id, idField.get().getType());
+            }
+            BeanClassProcessor.setFieldValue(idField.get(), instance, identifier);
+            registry.resolveReferences(id, instance);
+        });
+    }
+
+    private static Map<String, Class<?>> getAssumedTargetTypes(PendingReferenceRegistry registry) {
+        final Map<String, Class<?>> idsToTypes = new HashMap<>();
+        registry.getPendingReferences().forEach((id, refs) -> {
+            assert refs != null;
+            assert !refs.isEmpty();
+            final Optional<Class<?>> targetType =
+                    refs.stream().filter(ref -> ref.getTargetType().isPresent()).findFirst().flatMap(
+                            PendingReference::getTargetType);
+            if (targetType.isEmpty()) {
+                LOG.debug("No assumed target type found for reference with id '{}'. Skipping the reference.", id);
+            } else {
+                idsToTypes.put(id, targetType.get());
+            }
+        });
+        return idsToTypes;
+    }
+}
