afa16e9bbe1b34fdb129e27ac941000152a830a1
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2025-07-01 17:54:10 +0200

    Add tests for serialization/deserialization symmetry.

---
+++ src/test/java/cz/cvut/kbss/jsonld/integration/ContextBuildingSerializerSymmetryTest.java
@@ -0,0 +1,13 @@
+package cz.cvut.kbss.jsonld.integration;
+
+import cz.cvut.kbss.jsonld.Configuration;
+import cz.cvut.kbss.jsonld.serialization.JsonLdSerializer;
+import cz.cvut.kbss.jsonld.serialization.util.BufferedJsonGenerator;
+
+public class ContextBuildingSerializerSymmetryTest extends DeSerializationSymmetryTest {
+
+    @Override
+    JsonLdSerializer getSerializer(BufferedJsonGenerator jsonWriter, Configuration configuration) {
+        return JsonLdSerializer.createContextBuildingJsonLdSerializer(jsonWriter, configuration);
+    }
+}
