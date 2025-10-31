15a05d6dcc3ddc1a374d7f9bcc7871beb9c3d18d
Yaris van Thiel <yarisvanthiel@gmail.com>
2025-10-31 14:29:30 +0100

    Removed final

--- src/main/java/cz/cvut/kbss/jsonld/deserialization/expanded/ExpandedJsonLdDeserializer.java
+++ src/main/java/cz/cvut/kbss/jsonld/deserialization/expanded/ExpandedJsonLdDeserializer.java
@@ -34,7 +34,7 @@ import java.util.Map;
 
 public class ExpandedJsonLdDeserializer extends JsonLdDeserializer {
 
-	private final Map<String, Object> knownInstances;
+	private Map<String, Object> knownInstances;
 	private PendingReferenceRegistry referenceRegistry;
 
     public ExpandedJsonLdDeserializer() {
